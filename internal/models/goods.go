c71cc7e15fc9f79c07b6b77fe291d58ff8407e9b
blckvia <a.vyatkinn@icloud.com>
2024-04-13 18:28:59 +0300

    feat: create new structs for meta info

--- internal/models/goods.go
+++ internal/models/goods.go
@@ -16,7 +16,20 @@ type Goods struct {
 }
 
 type UpdateGoods struct {
+	Name        *string `json:"name" db:"name"`
 	Description *string `json:"description" db:"description"`
+}
+
+type GetAllGoodsResponse struct {
+	Meta  Meta    `json:"meta"`
+	Goods []Goods `json:"goods"`
+}
+
+type Meta struct {
+	Total   int `json:"total"`
+	Removed int `json:"removed"`
+	Limit   int `json:"limit"`
+	Offset  int `json:"offset"`
 }
 
 func (i UpdateGoods) Validate() error {
