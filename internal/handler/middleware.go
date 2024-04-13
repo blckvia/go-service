820046a5cc4173c36fbef201dfe299271be0611b
blckvia <a.vyatkinn@icloud.com>
2024-04-13 18:31:10 +0300

    fix: rewrite functions. context variables as projectId and goodsId is redundant

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -3,40 +3,44 @@ package handler
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-const goodsCtx = "id"
+const (
+	goodsCtx   = "id"
+	projectCtx = "project_id"
+)
 
 func GetGoodsId(c *gin.Context) (int, error) {
-	id, ok := c.Get(goodsCtx)
-	if !ok {
-		newErrorResponse(c, http.StatusNotFound, "goods id not found")
+	goodsIDStr := c.Param("id")
+	if goodsIDStr == "" {
+		newErrorResponse(c, http.StatusBadRequest, "Goods ID is required")
 		return 0, errors.New("goods id not found")
 	}
 
-	idInt, ok := id.(int)
-	if !ok {
-		newErrorResponse(c, http.StatusNotFound, "goods id not found")
+	goodsID, err := strconv.Atoi(goodsIDStr)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "Invalid Goods ID format")
 		return 0, errors.New("goods id not found")
 	}
 
-	return idInt, nil
+	return goodsID, nil
 }
 
 func GetProjectId(c *gin.Context) (int, error) {
-	id, ok := c.Get(goodsCtx)
-	if !ok {
-		newErrorResponse(c, http.StatusNotFound, "project id not found")
+	projectIDStr := c.Param("project_id")
+	if projectIDStr == "" {
+		newErrorResponse(c, http.StatusBadRequest, "Project ID is required")
 		return 0, errors.New("project id not found")
 	}
 
-	idInt, ok := id.(int)
-	if !ok {
-		newErrorResponse(c, http.StatusNotFound, "project id not found")
+	projectID, err := strconv.Atoi(projectIDStr)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "Invalid Project ID format")
 		return 0, errors.New("project id not found")
 	}
 
-	return idInt, nil
+	return projectID, nil
 }
