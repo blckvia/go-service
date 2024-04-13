974dfff49c626c003cae72a1b38c23f3d0919e91
blckvia <a.vyatkinn@icloud.com>
2024-04-13 18:29:39 +0300

    feat: add new deatiled error response

--- internal/handler/response.go
+++ internal/handler/response.go
@@ -14,7 +14,24 @@ type statusResponse struct {
 	Status string `json:"status"`
 }
 
+type detailedErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Detail  string `json:"detail"`
+}
+
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	logger.GetLogger().Error(message)
 	c.AbortWithStatusJSON(statusCode, errorResponse{message})
+}
+
+func newDetailedErrorResponse(c *gin.Context, statusCode, errorCode int, message, detail string) {
+	logger.GetLogger().Error(message)
+	c.AbortWithStatusJSON(statusCode, detailedErrorResponse{
+		Code:    errorCode,
+		Message: message,
+		Status:  statusCode,
+		Detail:  detail,
+	})
 }
