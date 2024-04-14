cd63957b8f16ec5c6422a0cf85e7f7e76fb8369c
blckvia <a.vyatkinn@icloud.com>
2024-04-14 16:14:44 +0300

    feat: activate project routes

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -19,21 +19,22 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	api := router.Group("/api")
 	{
-		//projects := api.Group("/projects")
-		//{
-		//	projects.POST("/", h.createProject)
-		//	projects.GET("/", h.getAllProjects)
-		//	projects.GET("/:id", h.getProject)
-		//	projects.PATCH("/:id", h.updateProject)
-		//	projects.DELETE("/:id", h.deleteProject)
-		//}
+		projects := api.Group("/projects")
+		{
+			projects.POST("/", h.createProject)
+			projects.GET("/", h.getAllProjects)
+			projects.GET("/:project_id", h.getProject)
+			projects.PATCH("/:project_id", h.updateProject)
+			projects.DELETE("/:project_id", h.deleteProject)
+		}
 
 		goods := api.Group("/goods")
 		{
 			goods.GET("/list", h.getAllGoods)
+			goods.PATCH("/prioritize/:project_id/:id", h.reprioritize)
 			goods.POST("/:project_id", h.createGoods)
 			goods.PATCH("/:project_id/:id", h.updateGoods)
-			goods.DELETE("/:id", h.deleteGoods)
+			goods.DELETE("/:project_id/:id", h.deleteGoods)
 			goods.GET("/:project_id/:id", h.getOne)
 		}
 	}
