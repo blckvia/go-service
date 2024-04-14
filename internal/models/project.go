ac801a7d9e060d7ddf94470cd18c0cbce0232e2b
blckvia <a.vyatkinn@icloud.com>
2024-04-14 16:15:35 +0300

    feat: add update struct & getAll struct

--- internal/models/project.go
+++ internal/models/project.go
@@ -15,9 +15,24 @@ type UpdateProject struct {
 	Name *string `json:"name" db:"name"`
 }
 
+type GetAllProjects struct {
+	Meta     MetaProjects `json:"meta"`
+	Projects []Project    `json:"projects"`
+}
+
+type UpdateProjects struct {
+	Name *string `json:"name" db:"name"`
+}
+
 func (i UpdateProject) Validate() error {
 	if i.Name == nil {
 		return errors.New("empty name")
 	}
 	return nil
+}
+
+type MetaProjects struct {
+	Total  int `json:"total"`
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
 }
