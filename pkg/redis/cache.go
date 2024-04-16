9cc97f10cdb392a132d5d628d14ac6d016d44ed4
blckvia <a.vyatkinn@icloud.com>
2024-04-16 15:24:52 +0300

    fix: add Int methods

--- pkg/redis/cache.go
+++ pkg/redis/cache.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -10,6 +11,9 @@ import (
 type Cache interface {
 	Get(ctx context.Context, key string) (string, error)
 	Set(ctx context.Context, key, value string, ttl time.Duration) error
+	Delete(ctx context.Context, key string) error
+	GetInt(ctx context.Context, key string) (int, error)
+	SetInt(ctx context.Context, key string, value int, ttl time.Duration) error
 }
 
 type RedisCache struct {
@@ -26,4 +30,20 @@ func (r *RedisCache) Get(ctx context.Context, key string) (string, error) {
 
 func (r *RedisCache) Set(ctx context.Context, key string, value string, expiration time.Duration) error {
 	return r.client.Set(ctx, key, value, expiration).Err()
+}
+
+func (r *RedisCache) Delete(ctx context.Context, key string) error {
+	return r.client.Del(ctx, key).Err()
+}
+
+func (r *RedisCache) GetInt(ctx context.Context, key string) (int, error) {
+	value, err := r.Get(ctx, key)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(value)
+}
+
+func (r *RedisCache) SetInt(ctx context.Context, key string, value int, expiration time.Duration) error {
+	return r.client.Set(ctx, key, strconv.Itoa(value), expiration).Err()
 }
