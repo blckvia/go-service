233f3e4467766e263f6c749f81253000c99031b5
blckvia <a.vyatkinn@icloud.com>
2024-04-21 11:44:31 +0300

    feat: add tracing for redis

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,16 +1,23 @@
 package redis
 
 import (
+	"github.com/redis/go-redis/extra/redisotel/v9"
 	"github.com/redis/go-redis/v9"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
-func NewClient() *redis.Client {
+func NewClient(logger *zap.Logger) *redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr:     viper.GetString("rdb.host") + ":" + viper.GetString("rdb.port"),
 		Password: viper.GetString("rdb.password"),
 		DB:       viper.GetInt("rdb.dbname"),
 	})
+
+	if err := redisotel.InstrumentTracing(client); err != nil {
+		logger.Fatal("failed to instrument redis tracing", zap.Error(err))
+		panic(err)
+	}
 
 	return client
 }
