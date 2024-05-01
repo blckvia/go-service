ed69f015518120e50531a9ed7f9ef26f593da522
blckvia <a.vyatkinn@icloud.com>
2024-05-01 13:47:21 +0300

    feat: add pprof and make it available in debug mode only

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel/trace"
 
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
@@ -14,10 +17,14 @@ import (
 
 type Handler struct {
 	services *service.Service
+	tracer   trace.Tracer
 }
 
-func New(services *service.Service) *Handler {
-	return &Handler{services: services}
+func New(services *service.Service, tracer trace.Tracer) *Handler {
+	return &Handler{
+		services: services,
+		tracer:   tracer,
+	}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
@@ -27,6 +34,13 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	debugMode := viper.GetBool("debug")
+	if debugMode {
+		pprof.Register(router, "/debug/pprof")
+	}
+
+	// TODO: /metrics or config telemetry /goods_metrics ?
+	// TODO: взять к6 утилиту и написать нагрузку для плофайлера
 	api := router.Group("/api")
 	{
 		projects := api.Group("/projects")
