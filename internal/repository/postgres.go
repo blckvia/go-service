e880016743e69f8920a1bb017e7d6deb17b1a743
blckvia <a.vyatkinn@icloud.com>
2024-05-01 13:50:22 +0300

    feat: add context for tracing

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/exaring/otelpgx"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const (
@@ -21,13 +22,26 @@ type Config struct {
 	SSLMode  string
 }
 
-func NewPostgresDB(ctx context.Context, config Config) (*pgx.Conn, error) {
+func NewPostgresDB(ctx context.Context, config Config) (*pgxpool.Pool, error) {
 	const op = "storage.postgres.New"
 	connString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", config.Host, config.Port, config.Username, config.DBName, config.Password, config.SSLMode)
-	conn, err := pgx.Connect(ctx, connString)
+
+	cfg, err := pgxpool.ParseConfig(connString)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
+		return nil, fmt.Errorf("create connection pool: %w", err)
 	}
+
+	cfg.ConnConfig.Tracer = otelpgx.NewTracer()
+
+	conn, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("connect to database: %w", err)
+	}
+
+	//conn, err := pgx.Connect(ctx, connString)
+	//if err != nil {
+	//	return nil, fmt.Errorf("%s: %w", op, err)
+	//}
 
 	err = conn.Ping(ctx)
 	if err != nil {
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,7 +3,9 @@ package repository
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/nats-io/nats.go"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 
 	"go-service/internal/models"
@@ -11,20 +13,20 @@ import (
 )
 
 type Projects interface {
-	Create(input models.Project) (int, error)
-	Update(projectID int, input models.UpdateProjects) error
-	Delete(projectID int) error
-	GetAll(limit, offset int) (models.GetAllProjects, error)
-	GetByID(projectID int) (models.Project, error)
+	Create(ctx context.Context, input models.Project) (int, error)
+	Update(ctx context.Context, projectID int, input models.UpdateProjects) error
+	Delete(ctx context.Context, projectID int) error
+	GetAll(ctx context.Context, limit, offset int) (models.GetAllProjects, error)
+	GetByID(ctx context.Context, projectID int) (models.Project, error)
 }
 
 type Goods interface {
-	Create(projectID int, goods models.Goods) (int, error)
-	Update(goodsID, projectID int, input models.UpdateGoods) error
-	Delete(goodsID, projectID int) error
-	GetAll(limit, offset int) (models.GetAllGoods, error)
-	GetOne(goodsID, projectID int) (models.Goods, error)
-	Reprioritize(goodsID, projectID int, priority int) error
+	Create(ctx context.Context, projectID int, goods models.Goods) (int, error)
+	Update(ctx context.Context, goodsID, projectID int, input models.UpdateGoods) error
+	Delete(ctx context.Context, goodsID, projectID int) error
+	GetAll(ctx context.Context, limit, offset int) (models.GetAllGoods, error)
+	GetOne(ctx context.Context, goodsID, projectID int) (models.Goods, error)
+	Reprioritize(ctx context.Context, goodsID, projectID int, priority int) error
 }
 
 type Repository struct {
@@ -32,9 +34,9 @@ type Repository struct {
 	Goods
 }
 
-func New(ctx context.Context, db *pgx.Conn, cache r.Cache, logger *zap.Logger) *Repository {
+func New(ctx context.Context, db *pgxpool.Pool, cache r.Cache, logger *zap.Logger, nats *nats.Conn, tracer trace.Tracer) *Repository {
 	return &Repository{
-		Goods:    NewGoodsPostgres(ctx, db, cache, logger),
-		Projects: NewProjectPostgres(ctx, db, cache, logger),
+		Goods:    NewGoodsPostgres(ctx, db, cache, logger, nats, tracer),
+		Projects: NewProjectPostgres(ctx, db, cache, logger, tracer),
 	}
 }
